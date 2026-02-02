edaebbc5fcfc1651f64a2a71afb464014eafbf7c
vmguard <thatguy3759@proton.me>
2026-02-01 21:27:44 -0500

    Add WindowCapture class for screen capturing

--- src/main.cpp
+++ src/main.cpp
@@ -63,3 +63,54 @@ HRESULT DwmpDxGetWindowSharedSurface(
     }
     return pfn(hwnd, luidAdapter, hmonitorAssociation, dwFlags, pfmtWindow, phDxSurface, puiUpdateId);
 }
+// window capture class
+class WindowCapture {
+private:
+    ID3D11Device* d3dDevice = nullptr;
+    ID3D11DeviceContext* d3dContext = nullptr;
+    ID3D11Texture2D* stagingTex = nullptr;
+    IWICImagingFactory* wicFactory = nullptr;
+
+    // Common GUIDs
+    const GUID GUID_WICPixelFormat32bppBGRA = { 0x6fddc324, 0x4e03, 0x4bfe, {0xb1, 0x85, 0x3d, 0x77, 0x76, 0x8d, 0xc9, 0x0f} };
+    const GUID GUID_WICPixelFormat24bppBGR = { 0x6fddc324, 0x4e03, 0x4bfe, {0xb1, 0x85, 0x3d, 0x77, 0x76, 0x8d, 0xc9, 0x0c} };
+
+    // Container formats
+    const GUID GUID_ContainerFormatPng = { 0x1b7cfaf4, 0x713f, 0x473c, {0xbb, 0xcd, 0x61, 0x37, 0x42, 0x5f, 0xae, 0xaf} };
+    const GUID GUID_ContainerFormatBmp = { 0xaf1d87e, 0xfcfe, 0x4188, {0xbd, 0xeb, 0xa7, 0x90, 0x64, 0x71, 0xcb, 0xe3} };
+    const GUID GUID_ContainerFormatJpeg = { 0x19e4a5aa, 0x5662, 0x4fc5, {0xa0, 0xc0, 0x17, 0x58, 0x2, 0x8e, 0x10, 0x57} };
+    const GUID GUID_ContainerFormatTiff = { 0x163bcc30, 0xe2e9, 0x4f0b, {0x96, 0x1d, 0xa3, 0xe9, 0xfd, 0xb7, 0x88, 0xa3} };
+    const GUID GUID_ContainerFormatGif = { 0x1f8a5601, 0x7d4d, 0x4cbd, {0x9c, 0x82, 0x1b, 0xc8, 0xd4, 0xee, 0xb9, 0xa5} };
+
+public:
+    WindowCapture() {}
+
+    ~WindowCapture() {
+        cleanup();
+    }
+
+    HRESULT initialize() {
+        HRESULT hr = D3D11CreateDevice(
+            nullptr,
+            D3D_DRIVER_TYPE_HARDWARE,
+            nullptr,
+            D3D11_CREATE_DEVICE_BGRA_SUPPORT,
+            nullptr,
+            0,
+            D3D11_SDK_VERSION,
+            &d3dDevice,
+            nullptr,
+            &d3dContext
+        );
+
+        if (SUCCEEDED(hr)) {
+            hr = CoCreateInstance(
+                CLSID_WICImagingFactory,
+                nullptr,
+                CLSCTX_INPROC_SERVER,
+                IID_PPV_ARGS(&wicFactory)
+            );
+        }
+
+        return hr;
+    }
